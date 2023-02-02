7babc7f2711e5766fe4d54718360790973a6071d
Qaisar Abbas <122449496+qaisar786abbas@users.noreply.github.com>
2023-02-02 20:33:10 +0500

    added files

---
+++ Js-5/app.js
@@ -0,0 +1,209 @@
+//*********CHAPTER 05 */
+//******MATH EXPRESSION
+
+//Question no 1
+
+// var num1 = 5;
+// var num2 = 4;
+// var add = 5 + 4;
+// document.write("<h3> Sum of 5 and 4 is 9 <h3>");
+
+// Question no 2
+ 
+// var num1 = 5;
+// var num2 = 4;
+// var sub = 5 - 4;
+// document.write("<h3> Sub of 5 and 4 is 1 <h3>");
+
+// var num1 = 5;
+// var num2 = 4;
+// var Mul = 5 * 4;
+// document.write("<h3> Mul of 5 and 4 is 20 <h3>");
+
+// var num1 = 8;
+// var num2 = 4;
+// var Div = 8 / 4;
+// document.write("<h3> Div of 5 and 4 is 2 </h3>");
+
+// var num1 = 5;
+// var num2 = 4;
+// var Mod = 5 % 4;
+// document.write("<h3> Mod of 5 and 4 is 1 </h3>");
+
+//Question no 3
+// (a)
+//var num ;
+//(b)
+// var num = 2;
+// document.write("<h2> value of variable is 2 </h2>")
+//(c)
+// var num = 3;
+// alert(num);
+//(d)
+// document.write("<h2> Initial value of variable is 3 </h2>");
+//(e)
+// var num = 3;
+// num = ++num;
+// alert(num);
+//(f)
+// document.write("<h2> Value after increment is 4 </h2>");
+//(g)
+// var num = 4;
+// num = num + 6;
+// alert(num);
+//(h)
+// document.write("<h2> Value after adding : 10 </h2>");
+//(i)
+// var num = 10;
+// num = --num;
+// alert(num);
+//(j)
+// document.write("<h2> Value after decrement is : 9 </h2>");
+//(k)
+// var num = 9;
+// num = 9 % 3;
+// alert(num);
+//(l)
+// document.write("<h2> The reminder value is 0 </h2>");
+
+
+//Question no 5
+// document.write("<h2> TABLE OF 4 </h2>");
+// document.write("<h2> 4 * 1 = 4 </h2>");
+// document.write("<h2> 4 * 2 = 8 </h2>");
+// document.write("<h2> 4 * 3 = 12 </h2>");
+// document.write("<h2> 4 * 4 = 16 </h2>");
+// document.write("<h2> 4 * 5 = 20 </h2>");
+// document.write("<h2> 4 * 6 = 24 </h2>");
+// document.write("<h2> 4 * 7 = 28 </h2>");
+// document.write("<h2> 4 * 8 = 32 </h2>");
+// document.write("<h2> 4 * 9 = 36 </h2>");
+// document.write("<h2> 4 * 10 = 40 </h2>");
+
+//Question no 6
+//(a)
+//var temp = 30c;
+//(b)
+// var temp = 30;
+// temp = (30 * 5 / 9) + 32;
+// alert(temp);
+// document.write("<h2> 30 celcius into fahrenheit is 48.66666666666667 </h2>");
+//(c)
+// var temp = 300;
+//(d)
+// var temp = 300;
+// temp = (300 - 32) * 5 / 9;
+// alert(temp);
+// document.write("<h2> 300 fah into celcius is 148.8888888888888889</h2>")
+
+//Question no 6
+//(a)
+// document.write("<h1> SHOPPING CARD </h1>");
+//(b)
+// var price = 650;
+// document.write("<h3> Price of 1 item is 650 </h3>");
+//(c)
+// document.write("<h3> Quantity of item 1 is 3 </h3>");
+//(d)
+// document.write("<h3> Price of item 2 is 100 </h3>");
+//(e)
+// document.write("<h3> Quantity of item 2 is 7 </h3>");
+//(f)
+// document.write("<h3> Shipping charges is 100 </h3>");
+
+// document.write("<h1> Total cost of your order is 2750 </h1>");
+
+//Question no 8
+ 
+//(a)
+// document.write("<h1> MARK SHEET </h1>");
+//(b)
+// var totalMark = 980;
+// document.write("<h3> Total mark = 980 </h3>");
+//(c)
+// var obtainMark = 804;
+// document.write("<h3> Obtain mark =804 </h3>");
+//(d)
+// var per = 804 / 980 * 100;
+// alert(per); 
+// document.write("<h3> Percentage = 82.0408163265306% </h3>")
+
+//Question no 9
+//(a)
+// document.write("<h1> CURRENCY IN PKR </h1>");
+//(b)
+// document.write("<h3> Total currency in PKR:1748 </h3>");
+//(c)
+// var dol = 1748 / 269;
+// alert(dol);
+// document.write("<h3> doller in pkr 6.4981413 </h3>");
+//(d)
+// var dinar = 1748 / 94;
+// alert(dinar);
+// document.write("<h3> dinar in pkr is 18.595745 </h3>");
+
+//question no 10
+// var num = 5;
+//(a)
+// num = num + 5;
+// alert(num);
+//(b)
+// num = num * 10;
+// alert(num);
+//(c)
+// num = 50 / 2;
+// alert(num);
+
+//Question no 11
+// document.write("<h1>Age Calculator</h1>");
+//(a)
+// var year = 2023;
+// document.write("<h2>Current year is 2023</h2>");
+//(b)
+// var year1 = 2003;
+// document.write("<h2>Birth Year is 2003</h2>");
+//(c)
+// year = year - year1;
+// alert(year);
+// document.write("<h2> The current age is 20 </h2>");
+
+//Question no 12
+// document.write("<h1>The Geometrizer</h1>");
+// (a)
+// var radius = 30;
+// document.write("<h2><i>Radius of a circle is: 30</i></h2>");
+//(b)
+// var radius = 30;
+// radius = 3.143 * 30 * 30;
+// alert(radius);
+// document.write("<h2>Area of a circle is: 2828.7</h2>");
+//(c)
+// radius = 2 * 3.143 * 30;
+// alert(radius);
+// document.write("<h2>Circimference of a circle is: 188.5799999</h2>")
+
+//Question no 13
+// document.write("<h1>The Lifetime Supply Calculator:</h1>");
+//(a)
+// var fav = "chocolate chip";
+// document.write("<h2>Favourite snake: Chocolate chip</h2>");
+//(b)
+// var age = 19;
+// document.write("<h2>Age is: 19</h2>");
+//(c)
+// document.write("<h2>Estimate maximum Age is: 65</h2>");
+//(d)
+// document.write("<h2>Amount of snake per day: 3</h2>");
+// document.write("<h2>You will need 150 chocoate chip to last you until the ripe old age of 65</h2>");
+
+
+
+
+
+
+
+
+
+
+
+
